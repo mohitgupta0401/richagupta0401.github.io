11979438a5aa8de38ea0b7bd382debcea075fb27
Richagupta0401 <41484368+Richagupta0401@users.noreply.github.com>
2018-07-20 22:50:28 +0530

    Add files via upload

--- staticpages/assets/js/google-apps-script.js
+++ staticpages/assets/js/google-apps-script.js
@@ -5,7 +5,7 @@
  ******************************************************************************/
 
 // if you want to store your email server-side (hidden), uncomment the next line
-// var TO_ADDRESS = "example@email.net";
+var TO_ADDRESS = "mohitgupta.engg@gmail.com";
 
 // spit out all the keys/values from the form in HTML for email
 // uses an array of keys if provided or the object to determine field order
